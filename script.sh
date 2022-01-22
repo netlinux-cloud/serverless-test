710a21c32f19bfc6c1a023db26168cd2cadc57e5
Graham <code@netlinux.co.uk>
2022-01-22 09:30:41 +0000

    Makefile

--- script.sh
+++ script.sh
@@ -17,8 +17,28 @@ id
 
 apk --no-cache add gcc g++ make
 
-gcc
+echo '#include <stdio.h>
+
+int main()
+{
+    puts ("Hello");
+
+    return 0;
+}
+' >test.cpp
+
+
+gcc test.cpp -o test
+./test
+rm -vf test
+
+echo '
+test: test.cpp
+    gcc test.cpp -o test
+' >Makefile
+
 make
+./test
 
 exit 0
 
